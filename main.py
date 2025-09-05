40f66ba1cc3b78588945d746c4eca6efb6282232
Rohith <skatt24@illinois.edu>
2025-09-04 22:19:19 -0500

    Boilerplate

--- main.py
+++ main.py
@@ -1,33 +1,6 @@
-import os
-import sys
-from dotenv import load_dotenv
-from google import genai
-from google.genai import types
+def main():
+    print("Hello from aiagent!")
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-client = genai.Client(api_key=api_key)
 
-if len(sys.argv) < 2:
-    print("I need a prompt")
-    sys.exit(1)
-
-verbose_flag = False
-if len(sys.argv) == 3 and sys.argv[2] == "--verbose":
-    verbose_flag = True
-
-prompt = sys.argv[1]
-
-messages = [
-    types.Content(role="user", parts=[types.Part(text=prompt)]),
-]
-
-response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents=messages
-)
-print(response.text)
-
-if verbose_flag:
-    print(f"User prompt: {prompt}")
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+if __name__ == "__main__":
+    main()
